84e5c9169ef0f96ecc84d28efd55249681831da0
Patrick Thomas <j4kp07@gmail.com>
2011-12-13 12:05:02 -0600

    FML

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
-	ini_set('register_globals', off);
+	ini_set('register_globals', 'off');
 	
 	date_default_timezone_set('America/Los_Angeles');
 	
