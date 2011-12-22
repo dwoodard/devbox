14dfdceecea9baba49e610238559cb3b80524ae8
Patrick Thomas <j4kp07@gmail.com>
2011-12-21 19:33:00 -0600

    remove nivo slider and pretty photo calls in header

--- views/allpaws/_header.php
+++ views/allpaws/_header.php
@@ -26,30 +26,7 @@
 				'assets/js/common',
 			), FALSE, TRUE);
 		?>
-		
-		<link rel="stylesheet" href="/assets/js/nivo-slider/themes/default/default.css" type="text/css" media="screen" />
-		<link rel="stylesheet" href="/assets/js/nivo-slider/nivo-slider.css" type="text/css" media="screen" />
-		<script type="text/javascript" src="/assets/js/nivo-slider/jquery.nivo.slider.js"></script>		
 
-		<link rel="stylesheet" href="/assets/css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" />
-		<script src="/assets/js/jquery.prettyPhoto.js" type="text/javascript"></script>
-		<script type="text/javascript">
-		  $(document).ready(function(){
-			$("a[rel^='prettyPhoto']").prettyPhoto({
-				animation_speed: 'normal', /* fast/slow/normal */
-				slideshow: 5000, /* false OR interval time in ms */
-				autoplay_slideshow: false, /* true/false */
-				opacity: 0.70, /* Value between 0 and 1 */
-				show_title: true, /* true/false */
-				allow_resize: true, /* Resize the photos bigger than viewport. true/false */
-				default_width: 500,
-				default_height: 344,
-				counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
-				theme: 'facebook' /* light_rounded / dark_rounded / light_square / dark_square / facebook */
-			});
-		  });
-		</script>
-		
 		<meta charset="utf-8">
 
 		<!--[if lt IE 7]> <link href="/assets/css/ie6.css" rel="stylesheet" type="text/css" /> <![endif]-->
